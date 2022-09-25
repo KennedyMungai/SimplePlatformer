81f40cb6c2765b1e7a713db79f0b173530779260
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-25 11:59:13 +0300

    Moved the movement code to its own function

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,13 +10,7 @@ const JUMP_SPEED = 3000;
 func _physics_process(delta: float) -> void:
 	apply_gravity();
 	jump();
-	
-	if(Input.is_action_pressed("left") and not Input.is_action_pressed("right")):
-		motion.x = -SPEED;
-	elif(Input.is_action_pressed("right") and not Input.is_action_pressed("left")):
-		motion.x = SPEED;
-	else: 
-		motion.x = 0;
+	move();
 		
 	move_and_slide(motion, Vector2.UP);
 
@@ -32,3 +26,11 @@ func jump() -> void:
 	if(Input.is_action_just_pressed("jump") and is_on_floor()):
 		motion.y -= JUMP_SPEED;
 	
+
+func move() -> void:
+	if(Input.is_action_pressed("left") and not Input.is_action_pressed("right")):
+		motion.x = -SPEED;
+	elif(Input.is_action_pressed("right") and not Input.is_action_pressed("left")):
+		motion.x = SPEED;
+	else: 
+		motion.x = 0;
