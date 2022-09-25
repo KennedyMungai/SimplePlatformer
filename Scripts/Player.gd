992fd1694a5fe6d94ff506579cf574036cfb8d08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-25 12:32:50 +0300

    Commented out all the ciode inside the animate function

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -40,14 +40,14 @@ func move() -> void:
 
 
 func animate() -> void:
-	if(motion.y < 0):
-		$PlayerAnimation.play("jump");
-	elif(motion.x > 0):
-		$PlayerAnimation.play("walk");
-		$PlayerAnimation.flip_h = false;
-	elif(motion.x < 0):
-		$PlayerAnimation.play("walk");
-		$PlayerAnimation.flip_h = true;
-	else:
-		$PlayerAnimation.play("idle");
+#	if(motion.y < 0):
+#		$PlayerAnimation.play("jump");
+#	elif(motion.x > 0):
+#		$PlayerAnimation.play("walk");
+#		$PlayerAnimation.flip_h = false;
+#	elif(motion.x < 0):
+#		$PlayerAnimation.play("walk");
+#		$PlayerAnimation.flip_h = true;
+#	else:
+#		$PlayerAnimation.play("idle");
 	
