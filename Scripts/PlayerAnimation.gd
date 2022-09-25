5feae2f68fd26a2b833961762780c7c145669b5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-25 12:42:41 +0300

    REmoved the reference to the PlayerANimation node

--- Scripts/PlayerAnimation.gd
+++ Scripts/PlayerAnimation.gd
@@ -4,12 +4,12 @@ extends AnimatedSprite
 
 func _on_Player_animate(motion) -> void:
 	if(motion.y < 0):
-		$PlayerAnimation.play("jump");
+		play("jump");
 	elif(motion.x > 0):
-		$PlayerAnimation.play("walk");
-		$PlayerAnimation.flip_h = false;
+		play("walk");
+		flip_h = false;
 	elif(motion.x < 0):
-		$PlayerAnimation.play("walk");
-		$PlayerAnimation.flip_h = true;
+		play("walk");
+		flip_h = true;
 	else:
-		$PlayerAnimation.play("idle");
+		play("idle");
