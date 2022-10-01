03357b3290dbcfc5574100fac610fce532439b80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-01 14:03:08 +0300

    Added a nul;l checker for the boost method

--- Scripts/JumpPad.gd
+++ Scripts/JumpPad.gd
@@ -4,5 +4,6 @@ extends Area2D
 
 
 func _on_JumpPad_body_entered(body: Node) -> void:
-	$AnimationPlayer.play("boost");
-	body.boost(); 
+	if(body.has_method("boost")):
+		$AnimationPlayer.play("boost");
+		body.boost(); 
