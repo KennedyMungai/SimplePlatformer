12a3582ad45f66f1b05ac2715db6c6d1a046beab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 17:19:05 +0300

    Added delettion code once the lightning goes offscreen

--- Scripts/Lightning.gd
+++ Scripts/Lightning.gd
@@ -4,3 +4,7 @@ extends Node2D
 func _ready() -> void:
 	set_as_toplevel(true);
 	global_position = get_parent().global_position;
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free();
