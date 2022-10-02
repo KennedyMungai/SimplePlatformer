c2024f3eec1ccda87dc160c0d88fae62885b4d4d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 12:05:02 +0300

    Made sure no one can take a coin more than once

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -5,9 +5,11 @@ var taken: bool = false;
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
-	$AnimationPlayer.play("Die");
-	$CoinSound.play();
-	get_tree().call_group("GameState", "coin_up");
+	if not taken:
+		taken = true;
+		$AnimationPlayer.play("Die");
+		$CoinSound.play();
+		get_tree().call_group("GameState", "coin_up");
 
 
 func die() -> void:
