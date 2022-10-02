01e7a8cbe7206488f48aa5a237c3572b17225960
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 11:57:17 +0300

    Added a call to the coin up function

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -6,6 +6,7 @@ extends Node2D
 func _on_Area2D_body_entered(body: Node) -> void:
 	$AnimationPlayer.play("Die");
 	$CoinSound.play();
+	get_tree().call_group("GameState", "coin_up");
 
 
 func die() -> void:
