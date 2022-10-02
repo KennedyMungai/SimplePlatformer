71cf309f5929023c92c28095506f70bf4a4989a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 17:21:13 +0300

    Added some movement logic for the lightning

--- Scripts/Lightning.gd
+++ Scripts/Lightning.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+const SPEED = 200;
+
 
 func _ready() -> void:
 	set_as_toplevel(true);
 	global_position = get_parent().global_position;
+
+
+func _process(delta: float) -> void:
+	position.y += SPEED;
 
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
