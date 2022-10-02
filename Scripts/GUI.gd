4a3736c6fdcbbbdc211db5368203199c06946fc9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 12:01:49 +0300

    Added the update_coins functuion to the GUI script

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -7,3 +7,7 @@ func _ready() -> void:
 
 func update_lives(lives_left) -> void:
 	$Control/TextureRect/HBoxContainer/LivesDisplay.text = str(lives_left);
+
+
+func update_coins(coins) -> void:
+	$Control/TextureRect/HBoxContainer/CoinsDisplay.text = str(coins);
