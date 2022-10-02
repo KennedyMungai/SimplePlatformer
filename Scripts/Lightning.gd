69ad12c588586e4cb2bc3671a78590fa76029738
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 17:26:36 +0300

    Added some test code

--- Scripts/Lightning.gd
+++ Scripts/Lightning.gd
@@ -15,3 +15,11 @@ func _process(delta: float) -> void:
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free();
+
+
+func manage_collision() -> void:
+	var collider = $Area2D.get_overlapping_bodies();
+	
+	for object in collider:
+		if object.name == "Player":
+			print("Contact made");
