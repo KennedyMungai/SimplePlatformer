0558fbe7018d35865f990a09a9762ab49cc37678
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 10:24:11 +0300

    Commented out the old code and called the hurt function inside the GameState group

--- Scripts/Hazards.gd
+++ Scripts/Hazards.gd
@@ -3,5 +3,6 @@ extends Area2D
 
 
 func _on_Hazards_body_entered(body: Node) -> void:
-	if(body.has_method("hurt")):
-		body.hurt();
+#	if(body.has_method("hurt")):
+#		body.hurt();
+	get_tree().call_group("GameState", "hurt");
