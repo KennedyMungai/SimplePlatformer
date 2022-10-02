91c4ae63fcc56598b779d1a249c859dc4dd0a474
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 12:16:27 +0300

    Updated the function name

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -5,6 +5,6 @@ func _ready() -> void:
 	$Control/TextureRect/HBoxContainer/LivesDisplay.text = "3";
 
 
-func update_lives(lives_left, coins_collected) -> void:
+func update_gui(lives_left, coins_collected) -> void:
 	$Control/TextureRect/HBoxContainer/LivesDisplay.text = str(lives_left);
 	$Control/TextureRect/HBoxContainer/CoinsDisplay.text = str(coins_collected);
