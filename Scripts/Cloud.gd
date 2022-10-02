52fee5cf7928e445b3f6983a65ed0e641268e705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 17:01:01 +0300

    Added some logic to the cloud script

--- Scripts/Cloud.gd
+++ Scripts/Cloud.gd
@@ -1,3 +1,10 @@
 extends Node2D
 
 
+func _process(delta: float) -> void:
+	if $Sprite/RayCast2D.is_colliding():
+		fire();
+		
+
+func fire() -> void:
+	add_child(load("res://Scenes/Lightning.tscn"));
